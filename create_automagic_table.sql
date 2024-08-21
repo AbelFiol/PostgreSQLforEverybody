b2df638f14484dfa571debe500d6329747fe970a
AbelFiol <135761646+AbelFiol@users.noreply.github.com>
2024-08-21 18:47:00 -0500

    Delete create_automagic_table.sql

--- create_automagic_table.sql
+++
@@ -1,6 +0,0 @@
--- Create the 'automagic' table to store ID, name, and height information.
-CREATE TABLE automagic (
-  id SERIAL PRIMARY KEY,  -- Auto-incrementing ID field.
-  name VARCHAR(32) NOT NULL,  -- Name field with a maximum of 32 characters, required.
-  height REAL NOT NULL  -- Height field as a floating-point number, required.
-);
